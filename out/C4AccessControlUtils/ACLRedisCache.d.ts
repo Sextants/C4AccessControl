d386509e77d23766aa664f88cc86a45e8f10f5d2
sextant <zbgqx1234@163.com>
2018-10-18 01:37:29 +0800

    Create repository

---
+++ out/C4AccessControlUtils/ACLRedisCache.d.ts
@@ -0,0 +1,13 @@
+import { ACLCache } from "../C4AccessControlTypes/C4AccessControlConfig";
+import { Redis } from "ioredis";
+/**
+ * Redis Cache
+ */
+export default class ACLRedisCache extends ACLCache {
+    private m_RedisClient;
+    constructor(redis: Redis);
+    init(): Promise<boolean>;
+    release(): Promise<void>;
+    getCache(key: string): Promise<string>;
+    setCache(key: string, value: any): Promise<boolean>;
+}
